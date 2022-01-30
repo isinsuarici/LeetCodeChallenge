9151cb91b013efe67c537df00ba3c1149761eca2
Işınsu Arıcı <isinsuu98@hotmail.com>
2022-01-30 23:10:52 +0300

    1512

---
+++ E_1512_NumberOfGoodPairs.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int sonuc = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i < j){
+                    sonuc++;
+                }
+            }
+        }   return sonuc;
+    }
+}

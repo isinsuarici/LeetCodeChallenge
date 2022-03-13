dd04cbedf36a258817ed6f1fc257adf9c0e41ab0
Işınsu Arıcı <44557162+isinsuarici@users.noreply.github.com>
2022-03-14 02:30:49 +0300

    Update E_1002_FindCommonCharacters.py

--- E_1002_FindCommonCharacters.py
+++ E_1002_FindCommonCharacters.py
@@ -1,10 +1,10 @@
-class Solution
-    def commonChars(self, words List[str]) - List[str]
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
         first=list(words[0])
-        for word in words[1]
+        for word in words[1:]:
             other=[]
-            for letter in word
-                if letter in first
+            for letter in word:
+                if letter in first:
                     other.append(letter)
                     first.remove(letter)
             first=other

acc7a1f87b2341e21a9ab08349d71b4d90389599
Işınsu Arıcı <isinsuu98@hotmail.com>
2022-03-13 23:52:15 +0300

    1002

---
+++ E_1002_FindCommonCharacters.py
@@ -0,0 +1,12 @@
+class Solution
+    def commonChars(self, words List[str]) - List[str]
+        first=list(words[0])
+        for word in words[1]
+            other=[]
+            for letter in word
+                if letter in first
+                    other.append(letter)
+                    first.remove(letter)
+            first=other
+            
+        return first

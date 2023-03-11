11a9f150b7c241909cc4fea24de72e6bb944a591
isinsuarici <isinsuu98@hotmail.com>
2023-03-11 22:33:27 +0300

    Q1588 added.

--- E_1588_SumOfAllOddLengthSubarrays.java
+++ E_1588_SumOfAllOddLengthSubarrays.java
@@ -1,18 +1,9 @@
-public class Solution {
+class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
         int sum=0;
-        int uzunluk=arr.length;
-           if(arr.length%2!=1) uzunluk=arr.length-1;
-           while (uzunluk % 2 == 1) {
-               for (int i = 0; i < arr.length; i++) {
-                   if (i + uzunluk < arr.length + 1) {
-                       for (int j = i; j < uzunluk + i; j++) {
-                           sum += arr[j];
-                       }
-                   }
-               }
-               uzunluk = uzunluk - 2;
-           }
-        return sum;
+        for(int i=0; i<arr.length; i++){
+            sum += (((i + 1) * (arr.length - i) + 1) / 2) * arr[i];
         }
+        return sum;
     }
+}
